e92aacd10fb806123c7e8e06333aac3e43368a7f
angelrene45 <ar.herrera0@gmail.com>
2022-07-20 15:14:23 -0500

    Add notebook

--- modules/process_request.py
+++ modules/process_request.py
@@ -5,12 +5,15 @@ from requests.models import Response
 from .encrypt_sha1 import encrypt_word
 from .convert_time import convert_seconds_into_legible_time
 
-def process_countries(response: Response) -> DataFrame:
+def process_countries(response: Response, encrypt_language: bool=True, delay: int=0) -> DataFrame:
     """
         Method that process reponse json 
         and get the info for every country
 
-        - languages is encrypted in SHA1
+        @param response: Instance of Response from requests library
+        @param encrypt_language: The column language is encrypted in SHA1 if the flag is True
+        @param delay: The method process fast but if you want delay the process you need tu put the seconds
+
     """
     df = DataFrame([], columns=['Region', 'City Name', 'Language', 'Time'])
 
@@ -25,9 +28,10 @@ def process_countries(response: Response) -> DataFrame:
         # convert values to string (every language separate by -)
         languages_string = '-'.join(languages.values())
         # encrypt languages
-        languages_string_sha1 = encrypt_word(languages_string)
+        languages_string_sha1 = encrypt_word(languages_string) if encrypt_language else languages_string
+        # simulate take more time for every row
+        if delay > 0: time.sleep(delay)
         # get total time processed
-        # time.sleep(0.01) # simulate take more time
         time_process = time.time() - start_time
         # convert time to legible string
         time_legible = convert_seconds_into_legible_time(time_process)
@@ -43,6 +47,9 @@ def process_countries(response: Response) -> DataFrame:
     return df
 
 def show_statistics(df: DataFrame):
+    """
+        Show info from dataframe
+    """
     # Use str.replace and pass a regex pattern to remove the characters and avoid 'ms' word
     df_time = df['Time'].str.replace(r'[^\d.]','', regex=True).astype(float)
     # show statistics from time in dataframe 
